bfa85296b3cb472dae03935586c4e9112b3c932c
AndreMatheus2 <am_arao@hotmail.com>
2022-12-24 11:21:23 -0300

    Corrigindo models

--- src/main/java/br/com/banco/model/Conta.java
+++ src/main/java/br/com/banco/model/Conta.java
@@ -1,16 +1,37 @@
 package br.com.banco.model;
 
-import lombok.*;
-
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
+import java.io.Serializable;
 
-@Entity
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Conta {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idConta;
+@Entity
+@Table(name = "conta")
+public class Conta implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long idConta;
+
+    @Column(name = "nome_responsavel")
     private String nomeResponsavel;
 
+    /*@OneToMany(mappedBy = "conta", cascade = CascadeType.ALL)
+    private List<Transferencia> transferencias;*/
+
+    /**
+     * Contrutores*/
+    public Conta() {
+
+    }
+
+    public Conta(Long idConta, String nomeResponsavel) {
+        this.idConta = idConta;
+        this.nomeResponsavel = nomeResponsavel;
+    }
 }
--- src/main/java/br/com/banco/model/Transferencia.java
+++ src/main/java/br/com/banco/model/Transferencia.java
@@ -1,25 +1,49 @@
 package br.com.banco.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
-
-@Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Transferencia {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+@Entity
+@Table(name = "transferencia")
+public class Transferencia implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long idTransferencia;
+
+    @Column(name = "data_transferencia")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dataTransferencia;
+
     private Double valor;
+
     private String tipo;
-    private String nomeOperadorTransacao;
 
-    private Integer contaId;
-    private String nomeOperadorTransferencia;
+    @Column(name = "nome_operador_transacao")
+    private String operadorTransacao;
 
+
+    @Column(name = "conta_id")
+    private Long contaId;
+
+    /**
+     * Constructors*/
+    public Transferencia(){
+    }
+
+    public Transferencia(Long idTransferencia, Date dataTransferencia, Double valor, String tipo, String operadorTransacao, Long contaId) {
+        this.idTransferencia = idTransferencia;
+        this.dataTransferencia = dataTransferencia;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.operadorTransacao = operadorTransacao;
+        this.contaId = contaId;
+    }
 }
